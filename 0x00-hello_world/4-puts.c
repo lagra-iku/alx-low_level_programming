2978e1d7608510895cf45d4e27bae76ff29bad9c
Grace Nweke <lagra2001@gmail.com>
2023-02-16 16:26:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-/*
- * A C program that prints a string 
- */
+/* A C program that prints a string */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
