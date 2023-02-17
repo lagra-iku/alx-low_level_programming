19c775534d153221d4c5db288665e659f47c1abb
Grace Nweke <lagra2001@gmail.com>
2023-02-16 16:17:14 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-int main()
+/*
+ * A C program that prints "Programming is like building a multilingual puzzle
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
