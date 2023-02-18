6bcb326dbd43ea06ded5b731bd270dda30309547
Grace Nweke <lagra2001@gmail.com>
2023-02-17 16:59:26 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check polarity of a number
+ *
+ * Return: number polarity
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
