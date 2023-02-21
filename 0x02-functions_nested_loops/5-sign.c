c33d0884c5a45b2a94d8bd48260d8447085acf22
Grace Nweke <lagra2001@gmail.com>
2023-02-21 15:35:28 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - hecks for alphabetic character.
+ * @n: number to be checked
+ * Return: always 0 (success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} elseif(n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+		_putchar('0');
+		return (0);
+	}
+}
