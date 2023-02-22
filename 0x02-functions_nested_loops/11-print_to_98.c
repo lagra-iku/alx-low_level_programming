95c8708155f8bbed106d50a55f2ce392c534dae2
Grace Nweke <lagra2001@gmail.com>
2023-02-21 16:12:51 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: number to be checked
+ * Return: always 0 (success)
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		_putchar(n);
+		if (n < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++
+	}
+}
