54cf7e790838de1e07a286bb699445a9aa93185b
Grace Nweke <lagra2001@gmail.com>
2023-02-23 19:34:59 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,10 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (i == n)
-				_putchar('\\');
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
