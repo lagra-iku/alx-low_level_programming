c60656c2c53a4ccea59d25fe1095423f66ddee56
Grace Nweke <lagra2001@gmail.com>
2023-02-27 14:38:59 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: character
+ * Return: 0
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + 1); i++)
+		return (i);
+}
+
