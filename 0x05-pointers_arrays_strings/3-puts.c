cc7d83c0b3d115ae70ab22744107280680d5c1da
Grace Nweke <lagra2001@gmail.com>
2023-02-27 14:56:02 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: character to be manipulated
+ * Return: 0
+*/
+void _puts(char *str);
+{
+	int i = 0;
+
+	for (; *str++;)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
+
