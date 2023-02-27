4b62682d2e602cb9a172efc9b6e9de2c361c33b8
Grace Nweke <lagra2001@gmail.com>
2023-02-27 12:38:40 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - take a pointer to an int as parameter and updates the value
+ * @n: Pointer to be replicated
+ * Return: 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar(n);
+	_putchar('\n');
+}
