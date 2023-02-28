03d741a418d9f2cca4ed4f81ba49aa0e2a96d53e
Grace Nweke <lagra2001@gmail.com>
2023-02-27 18:25:51 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,19 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	int j, k;
+	char temp;
 
 	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
 		i++;
 	}
-	_putchar('\n');
-	for (i = i - 1; i >= 0; i--)
+	j = i - 1;
+	for (k = 0; k >= i / 2; k++)
 	{
-		_putchar(s[i]);
+		temp = s[k];
+		s[k] = s[j];
+		s[j--] = temp;
 	}
 }
 
