8e9bc40b7bf13355c49f7e64199c92fc7e42fce0
Grace Nweke <lagra2001@gmail.com>
2023-02-27 18:08:10 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a);
+		printf("%d", a[i]);
 		if (i != n - 1)
 			printf(", ");
 	}
-	printf('\n');
+	_putchar('\n');
 }
