dfe43037a20e0f4a51a2282177ec341a44c98aff
Grace Nweke <lagra2001@gmail.com>
2023-03-02 15:14:24 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncpy -  copies a string.
+ * @dest: destination
+ * @src: source
+ * @n: max no of bytes
+ * Return: 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0'
+	}
+	return (dest);
+}
