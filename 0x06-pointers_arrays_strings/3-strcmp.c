fb0dc41cb0231147496a8324a1a2a5a1ce436864
Grace Nweke <lagra2001@gmail.com>
2023-03-02 15:36:42 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcmp -  compares two strings.
+ * @s1: destination
+ * @s2: source
+ * Return: 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			return (0);
+		else
+			return (s1[i] - s2[i]);
+	}
+}
