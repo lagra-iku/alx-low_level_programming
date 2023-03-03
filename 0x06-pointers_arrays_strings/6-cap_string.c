a072d341d9d544e2aa03b3846cab65c1ff6f102e
Grace Nweke <lagra2001@gmail.com>
2023-03-02 18:17:52 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,11 +7,17 @@
 char *cap_string(char *s)
 {
 	int i;
+	char sep[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] == ' ' || s[i] == '.') && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] = s[i + 1]  - 32;
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if ((s[i] == sep[j]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+				s[i + 1] = s[i + 1]  - 32;
+			if (s[0] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[0] = s[0]  - 32;
+		}
 	}
 	return (s);
 }
