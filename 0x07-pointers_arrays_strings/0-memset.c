602f191fa6da904f960505e0b65c07b5c0a9d4a2
Grace Nweke <lagra2001@gmail.com>
2023-03-06 12:59:10 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: no of times to be manipulated
+ * Return: 0
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src;
+	return (dest);
+}
+
