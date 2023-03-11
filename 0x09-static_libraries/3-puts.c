94be32363f0806c921c1262dc59d8a0cb3ee5c52
Grace Nweke <lagra2001@gmail.com>
2023-03-10 19:33:33 -0800

    editing put file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout.
- * @str: character to be manipulated
+ * @s: character to be manipulated
  * Return: 0
 */
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i = 0;
 
-	for (; *str++;)
+	for (; *s++;)
 	{
-		_putchar(*(str - 1));
+		_putchar(*(s - 1));
 		i++;
 	}
 	_putchar('\n');
