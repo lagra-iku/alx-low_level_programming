ed63c0967ee6458b58c6a21c3ec428f5df3394f4
Grace Nweke <lagra2001@gmail.com>
2023-03-10 19:41:34 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name + newline
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguments of argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
