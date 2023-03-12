7e5821a982f88db2de7620e573f749647a997e93
Grace Nweke <lagra2001@gmail.com>
2023-03-12 16:43:10 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,25 +2,28 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- * main - multiplies two numbers.
+ * main - sum numbers.
  * @argc: count of arguments
  * @argv: array of argc
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc < 1)
 		return (0);
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
 		sum += atoi(argv[i]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
