aa747851d9db0280edcd0f0c6e439161da73ab05
Grace Nweke <lagra2001@gmail.com>
2023-03-11 16:03:57 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,17 +10,20 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc < 1)
-		return (0);
-	for (i = 1; i < argc; i++)
+	if (argc == 1)
+		printf("%d\n", 0);
+	else
 	{
-		if (!atoi(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (!atoi(argv[i]))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+			sum += atoi(argv[i]);
 		}
-		sum += atoi(argv[i]);
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
 	return (0);
 }
