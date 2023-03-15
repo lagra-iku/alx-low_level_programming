5db50fc0904f088b2a57327d4887db9962d8a2b6
Grace Nweke <lagra2001@gmail.com>
2023-03-14 19:48:42 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,9 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k = 0, size;
 	char *s_concat;
 
-	if (s1 == NULL || s2 == NULL)
+	/*if (s1 == NULL || s2 == NULL)
 		s1 = s2 = "";
+		*/
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
