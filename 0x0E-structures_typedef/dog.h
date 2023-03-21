122cc5226c0beefc4160b2b9687e410d0d7d77ab
Grace Nweke <lagra2001@gmail.com>
2023-03-20 19:18:15 -0700

    Creating dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_TYPE
+#define DOG_TYPE
+
+/**
+ * struct dog - type dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
