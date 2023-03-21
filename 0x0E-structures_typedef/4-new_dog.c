33816453900044dcbca0ba04480fd2f003d25319
Grace Nweke <lagra2001@gmail.com>
2023-03-20 20:49:36 -0700

    creating 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - type dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: dog
+ *
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_name;
+	/**
+	 * Still working on getting the correct code
+	 */
+	if (name == NULL || owner == NULL)
+		return (NULL);
+}
