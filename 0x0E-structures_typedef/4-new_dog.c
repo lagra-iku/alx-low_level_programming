e7f6f8ad4beb4ba4ac4d6bb7441040b11a399478
Grace Nweke <lagra2001@gmail.com>
2023-04-15 17:16:44 -0700

    updating 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -11,10 +12,19 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_name;
-	/**
-	 * Still working on getting the correct code
-	 */
-	if (name == NULL || owner == NULL)
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	dog->name = malloc(_strlen(name) + 1);
+	dog->owner = malloc(_strlen(owner) + 1);
+	if (dog->name == NULL || dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
 		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,12 @@ char *owner;
 
 typedef struct dog dog_t;
 
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 
 #endif
