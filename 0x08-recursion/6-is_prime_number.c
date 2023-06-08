714f1d2aed4042c6c7b60666f523eb8493451008
Grace Nweke <lagra2001@gmail.com>
2023-06-07 17:37:19 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _find_prime - checks to see if number is prime no
+ * @n: number to check
+ * @a: base value to compare
+ * Return: int prime or not
+ */
+int _find_prime(int n, int a)
+{
+	if (n < 2 || n % a == 0)
+		return (0);
+	else if (a > n / 2)
+		return (1);
+	else
+		return (_find_prime(n, a + 1));
+}
+/**
+ * is_prime_number - determine if number is prime no
+ * @n: number to check
+ * Return: 1
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (_find_prime(n, 2));
+}
