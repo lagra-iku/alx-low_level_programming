042a75d9167bf5bb67c0d3e973a514d4861df97b
Grace Nweke <lagra2001@gmail.com>
2023-06-21 15:05:32 -0700

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,16 @@
+/**
+  * get_op_func - the func that selects the correct func to perform the opn
+  * @s: operator passed as argument to the program
+  * Return:a pointer to the func that corresponds to the operator
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {{ "+", op_add }, { "-", op_sub }, { "*", op_mul },
+		{ "/", op_div }, { "%", op_mod }, { NULL, NULL }};
+	int i;
+
+	for (i = 0; ops[i].op != NULL && *(ops[i].op) != *s; i++)
+	{
+	}
+	return (ops[i].f);
+}
