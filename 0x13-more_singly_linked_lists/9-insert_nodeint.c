c64f9e82ed2ee040d6dc76d381f36ea44f80a794
Grace Nweke <lagra2001@gmail.com>
2023-07-05 15:28:44 -0700

    correcting task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = *head;
 		for (i = 0; i < idx - 1; i++)
+		{
 			temp = temp->next;
+		}
 		if (temp == NULL)
 			return (NULL);
 	}
