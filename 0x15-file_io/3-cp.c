0750e0f3303a0d916b6aeea30b261b967e4627ea
Grace Nweke <lagra2001@gmail.com>
2023-07-10 18:20:32 -0700

    correcting

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,39 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 #define BUFFER_SIZE 1024
-
-/**
- * open_error - checks for error while opening file
- * @openfrom: value if file could open
- * @file_from: name of file
- * Return: 98
-*/
-int open_error(int openfrom, char *file_from)
-{
-	if (openfrom == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-}
-
-/**
- * write_error - checks for error whilewriting to file
- * @writeto: value if file could be written to
- * @file_to: name of file to write to
- * @openfrom: value if file could open
- * Return: 98
-*/
-int write_error(int writeto, char *file_to, int openfrom)
-{
-	if (writeto == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(openfrom);
-		exit(99);
-}
-
 /**
  * main - main program
  * @argc: count of argument
@@ -54,9 +24,14 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	openfrom = open(file_from, O_RDONLY);
-	open_error(openfrom, file_from);
+	if (openfrom == -1)
+	{
+                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+                exit(98);
+        }
+	
 	writeto = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	write_error(writeto, file_to, openfrom);
+	if (writeto == -1) {dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);close(openfrom);exit(99);}
 	while ((lettersread = read(openfrom, buffer, BUFFER_SIZE)) > 0)
 	{
 		letterswritten = write(writeto, buffer, lettersread);
@@ -68,7 +43,7 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (bytes_read == -1)
+	if (lettersread == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		close(openfrom);
